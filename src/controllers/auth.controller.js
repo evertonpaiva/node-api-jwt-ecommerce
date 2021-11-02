13036b700999aadaec3c3c6e26aa2b81f2b41c6c
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-02 09:28:42 -0300

    Issue #6. Removendo código não utilizado.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 const db = require('../models');
 const config = require('../config/auth.config');
 const User = db.user;
-const Role = db.role;
 
 const Op = db.Sequelize.Op;
 
@@ -16,24 +15,7 @@ exports.signup = (req, res) => {
     password: bcrypt.hashSync(req.body.password, 8),
   })
     .then((user) => {
-      if (req.body.roles) {
-        Role.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.roles,
-            },
-          },
-        }).then((roles) => {
-          user.setRoles(roles).then(() => {
-            res.send({ message: 'User registered successfully!' });
-          });
-        });
-      } else {
-        // user role = 1
-        user.setRoles([1]).then(() => {
-          res.send({ message: 'User registered successfully!' });
-        });
-      }
+      res.send({ message: 'User registered successfully!' });
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
