dc58548e05f3667605a47c8f6d51e47677c5b22d
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-02 12:39:56 -0300

    Issue #6. Definindo campos como obrigatórios.

--- src/models/deal.model.js
+++ src/models/deal.model.js
@@ -3,37 +3,47 @@ module.exports = (sequelize, Sequelize) => {
     type: {
       type: Sequelize.ENUM,
       values: ['1 - Venda', '2 - Troca', '3 - Desejo'],
+      allowNull: false,
     },
     value: {
       type: Sequelize.DOUBLE,
+      allowNull: false,
     },
     description: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     trade_for: {
       type: Sequelize.STRING,
     },
     lat: {
       type: Sequelize.DOUBLE,
+      allowNull: false,
     },
     lng: {
       type: Sequelize.DOUBLE,
+      allowNull: false,
     },
     address: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     city: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     state: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     zip_code: {
       type: Sequelize.INTEGER,
+      allowNull: false,
     },
     urgency_type: {
       type: Sequelize.ENUM,
       values: ['1 - Baixa', '2 - Média', '3 - Alta', '4 - Data'],
+      allowNull: false,
     },
     urgency_limit_date: {
       type: Sequelize.DATE,
