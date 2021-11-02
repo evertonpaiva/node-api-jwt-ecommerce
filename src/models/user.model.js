c3d7f94d6d31c33acbaa372db8ee2ff876f6a4ab
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-02 12:40:20 -0300

    Issue #5. Definindo campos como obrigatórios.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,33 +2,43 @@ module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define('users', {
     name: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     email: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     login: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     password: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     lat: {
       type: Sequelize.DOUBLE,
+      allowNull: false,
     },
     lng: {
       type: Sequelize.DOUBLE,
+      allowNull: false,
     },
     address: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     city: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     state: {
       type: Sequelize.STRING,
+      allowNull: false,
     },
     zip_code: {
       type: Sequelize.INTEGER,
+      allowNull: false,
     },
   });
   return User;
