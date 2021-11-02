10e2ae661fde966340e403302af009961016dd4a
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-02 08:28:30 -0300

    Issue #6. Implementando model, controller e route de Negociação.

---
+++ src/controllers/deal.controller.js
@@ -0,0 +1,52 @@
+const db = require('../models');
+const Deal = db.deal;
+
+// Format deal data
+formatDeal = (deal) => {
+  return {
+    id: deal.id,
+    type: deal.type,
+    value: deal.value,
+    description: deal.description,
+    trade_for: deal.trade_for,
+    location: {
+      lat: deal.lat,
+      lng: deal.lng,
+      address: deal.address,
+      city: deal.city,
+      state: deal.state,
+      zip_code: deal.zip_code,
+    },
+    urgency: {
+      type: deal.urgency_type,
+      limit_date: deal.urgency_limit_date,
+    },
+    photos: deal.photos,
+  };
+};
+
+// Find a deal by id
+exports.findOne = (req, res) => {
+  console.log('AQUI');
+  const id = req.params.id;
+
+  Deal.findByPk(id)
+    .then((data) => {
+      if (data) {
+        formattedDeal = formatDeal(data);
+
+        res.send({
+          deal: formattedDeal,
+        });
+      } else {
+        res.status(404).send({
+          error: 'Deal with id=' + id + ' not found.',
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: 'Error retrieving Deal with id=' + id,
+      });
+    });
+};
---
+++ src/models/deal.model.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, Sequelize) => {
+  const Deal = sequelize.define('deals', {
+    type: {
+      type: Sequelize.ENUM,
+      values: ['1 - Venda', '2 - Troca', '3 - Desejo'],
+    },
+    value: {
+      type: Sequelize.DOUBLE,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
+    trade_for: {
+      type: Sequelize.STRING,
+    },
+    lat: {
+      type: Sequelize.DOUBLE,
+    },
+    lng: {
+      type: Sequelize.DOUBLE,
+    },
+    address: {
+      type: Sequelize.STRING,
+    },
+    city: {
+      type: Sequelize.STRING,
+    },
+    state: {
+      type: Sequelize.STRING,
+    },
+    zip_code: {
+      type: Sequelize.INTEGER,
+    },
+    urgency_type: {
+      type: Sequelize.ENUM,
+      values: ['1 - Baixa', '2 - Média', '3 - Alta', '4 - Data'],
+    },
+    urgency_limit_date: {
+      type: Sequelize.DATE,
+    },
+    photos: {
+      type: Sequelize.ARRAY(Sequelize.TEXT),
+    },
+  });
+  return Deal;
+};
