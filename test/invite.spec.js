a523a8c69b0042dffd8df3c14e030e779496dc7c
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-06 23:37:17 -0300

    Issue #14. Adicionado funções testes para listagem por id e atualização de convites.

--- test/invite.spec.js
+++ test/invite.spec.js
@@ -5,6 +5,54 @@ const { expect } = chai;
 const { API_URL, API_ROOT } = require('./config');
 
 describe('Invite test', () => {
+  //List
+  describe('List invite by user and id', () => {
+    describe('/api/v{n}/users/{ID}/invites/{ID} GET', () => {
+      it('Success request', (done) => {
+        request.get(
+          {
+            url: `${API_URL}${API_ROOT}/users/1/invites/1`,
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          },
+          (err, response, body) => {
+            const { invite } = JSON.parse(response.body);
+            // status code expected
+            expect(response.statusCode).to.equal(200);
+
+            // data expected
+            expect(invite).to.be.an('object');
+            expect(invite.name).to.equal('Monteiro Lobato');
+
+            done();
+          }
+        );
+      });
+      it('Fail request', (done) => {
+        request.get(
+          {
+            url: `${API_URL}${API_ROOT}/users/1/invites/99`,
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          },
+          (err, response, body) => {
+            const { error } = JSON.parse(response.body);
+
+            // status code expected
+            expect(response.statusCode).to.equal(404);
+
+            // data expected
+            expect(error).to.not.be.null;
+
+            done();
+          }
+        );
+      });
+    });
+  });
+
   // List all invites by user
   describe('List all invites by user', () => {
     describe('/api/v{n}/users/{ID}/invites GET', () => {
@@ -98,6 +146,63 @@ describe('Invite test', () => {
             const { error } = JSON.parse(response.body);
             // status code expected
             expect(response.statusCode).to.equal(500);
+
+            // data expected
+            expect(error).to.not.be.null;
+
+            done();
+          }
+        );
+      });
+    });
+  });
+
+  // Update
+  describe('Update invite', () => {
+    describe('/api/v{n}/users/1/invites/{ID} PUT', () => {
+      it('Success request', (done) => {
+        const newName = 'Rubens Barrichello';
+        request.put(
+          {
+            url: `${API_URL}${API_ROOT}/users/1/invites/1`,
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+            form: {
+              name: newName,
+            },
+          },
+          (err, response, body) => {
+            const { invite, error } = JSON.parse(response.body);
+
+            // status code expected
+            expect(response.statusCode).to.equal(200);
+
+            // data expected
+            expect(invite).to.be.an('object');
+            expect(invite.name).to.equal(newName);
+
+            done();
+          }
+        );
+      });
+      it('Fail request', (done) => {
+        const newDescription = 'Pagamento via cheque';
+        request.put(
+          {
+            url: `${API_URL}${API_ROOT}/users/1/invites/99`,
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+            form: {
+              description: newDescription,
+            },
+          },
+          (err, response, body) => {
+            const { error } = JSON.parse(response.body);
+
+            // status code expected
+            expect(response.statusCode).to.equal(404);
 
             // data expected
             expect(error).to.not.be.null;
