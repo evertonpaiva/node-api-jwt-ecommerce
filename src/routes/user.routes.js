5306ec34fc44caa786d04a1ef47ffae01f7cc412
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-06 23:36:23 -0300

    Issue #14. Corrigindo comentário.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ module.exports = function userRoute(app) {
     next();
   });
 
-  // get user by id
+  // Routes
   app.get('/api/v1/users/:id', [authJwt.verifyToken], controller.findOne);
 
   // create user
